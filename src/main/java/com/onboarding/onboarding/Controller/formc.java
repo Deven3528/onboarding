8b379afaaaa7de379ea1b6d9ec84be33b43dbc00
devem3528 <thoratdeven319@gmail.com>
2023-09-30 23:06:45 +0530

    sjsjs

--- src/main/java/com/onboarding/onboarding/Controller/formc.java
+++ src/main/java/com/onboarding/onboarding/Controller/formc.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.thymeleaf.context.Context;
+
 
 import com.onboarding.onboarding.Dao.UserData;
 import com.onboarding.onboarding.Dao.UserDataRepository;
-import com.onboarding.onboarding.email.EmailService;
+
 import com.onboarding.onboarding.service.UTMGenerator;
 
 
@@ -19,8 +19,7 @@ public class formc {
 
     @Autowired
     private UserDataRepository userRepository;
-    @Autowired
-    private EmailService emailService;
+    
 
 
     private String baseUtmLink = "http://localhost:8006/redirect";
@@ -47,17 +46,8 @@ public class formc {
              String[] nameParts = fullName.split(" ");
              String firstName = nameParts[0];
              model.addAttribute("userName", firstName);
-             model.addAttribute("utmLink", utmLink);
-             model.addAttribute("email",email);
              
-             //send the mail
-                Context context = new Context();
-                context.setVariable("userName", fullName);
-                context.setVariable("utmLink", utmLink);
-                context.setVariable("email", email);
-
-                emailService.sendWelcomeEmail("thoratdeven319@gmail.com", "welcome to Community","email",context);
-           
+             
             
              userRepository.save(user);
  
